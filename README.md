c0af386f5bed9fa9bb95a48f0f77de1fbfab47b3
tulians <julianailan@outlook.com>
2018-11-19 08:46:42 -0300

    Worked on: 1- Modularization. 2- Traffic difference among years using boxplots.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# traffic
+# Traffic prediction in AUSA tolls
+
+This work aims to predict the traffic passing by AUSA toll booths in 2019 and onwards.
+
+## Datasets
+The datasets can be found in data.buenosaires.gob.ar, among many other Buenos Aires city related datasets.
+
+[WIP]
--- flow.R
+++ flow.R
@@ -1,4 +1,4 @@
-# Traffic flow analysis in AUSA tolls.
+# Traffic flow analysis in AUSA toll booths.
 # Julián Ailán - jailan@itba.edu.ar
 # ITBA - 2018/2019
 
@@ -6,206 +6,52 @@ rm(list = ls()); gc();
 library(dplyr)
 
 setwd('~/Documents/traffic/')
+source('utils.R')
 
 #### Input data ####
-# Files from 2008 to 2018 have different column structure.
-traffic.2012.file <- './datasets/flujo-vehicular-por-unidades-de-peaje-ausa/flujo-vehicular-2012.csv'
-traffic.2014.file <- './datasets/flujo-vehicular-por-unidades-de-peaje-ausa/flujo-vehicular-2014.csv'
-traffic.2016.file <- './datasets/flujo-vehicular-por-unidades-de-peaje-ausa/flujo-vehicular-2016.csv'
-traffic.2017.file <- './datasets/flujo-vehicular-por-unidades-de-peaje-ausa/flujo-vehicular-2017.csv'
-traffic.2018.file <- './datasets/flujo-vehicular-por-unidades-de-peaje-ausa/flujo-vehicular-2018.csv'
-traffic.2012 <- read.csv(traffic.2012.file, sep = ';', header = T, stringsAsFactors = T)
-traffic.2014 <- read.csv(traffic.2014.file, sep = ';', header = T, stringsAsFactors = T)
-traffic.2016 <- read.csv(traffic.2016.file, sep = ';', header = T, stringsAsFactors = T)
-traffic.2017 <- read.csv(traffic.2017.file, sep = ';', header = T, stringsAsFactors = T)
-traffic.2018 <- read.csv(traffic.2018.file, sep = ',', header = T, stringsAsFactors = T)
-traffic.2012 <- traffic.2012[c('PERIODO', 'FECHA', 'DIA', 'HORA', 'HORA_FIN', 'ESTACION', 'TIPOVEHICULO', 'FORMA_PAGO', 'CANTIDAD_PASOS')]
-traffic.2014 <- traffic.2014[c('PERIODO', 'FECHA', 'DIA', 'HORA', 'HORAFIN', 'ESTACION', 'TIPOVEHICULO', 'FORMAPAGO', 'CANTIDADPASOS')]
-traffic.2016 <- traffic.2016[c('PERIODO', 'FECHA', 'DIA', 'HORA', 'HORA_FIN', 'ESTACION', 'TIPO_VEHICULO', 'FORMA_PAGO', 'CANTIDAD_PASOS')]
-traffic.2017 <- traffic.2017[c('PERIODO', 'FECHA', 'DIA', 'HORA', 'HORA_FIN', 'ESTACION', 'TIPO_VEHICULO', 'FORMA_PAGO', 'CANTIDAD_PASOS')]
-traffic.2018 <- traffic.2018[c('periodo', 'fecha', 'dia', 'hora', 'hora_fin', 'estacion', 'tipo_vehiculo', 'forma_pago', 'cantidad_pasos')]
-write.table(traffic.2012, file = traffic.2012.file, row.names = F, sep = ';')
-write.table(traffic.2014, file = traffic.2014.file, row.names = F, sep = ';')
-write.table(traffic.2016, file = traffic.2016.file, row.names = F, sep = ';')
-write.table(traffic.2017, file = traffic.2017.file, row.names = F, sep = ';')
-write.table(traffic.2018, file = traffic.2018.file, row.names = F, sep = ';')
-rm(traffic.2012, traffic.2012.file, traffic.2014, traffic.2014.file, 
-   traffic.2016, traffic.2016.file, traffic.2017, traffic.2017.file, 
-   traffic.2018, traffic.2018.file)
-
-# Merged all .csv files into one using awk rather than R packages.
-# awk 'FNR==1 && NR!=1{next;}{print}' *.csv > ~/Documents/final/datasets/merged.csv
-system('awk "FNR==1 && NR!=1{next;}{print}" ./datasets/flujo-vehicular-por-unidades-de-peaje-ausa/*.csv > ./datasets/merged.csv')
-merged <- './datasets/merged.csv'
-traffic <- './datasets/traffic.csv'
-df <- read.csv(merged, sep = ';', header = T, stringsAsFactors = T)
+df <- standarize.criteria()
 
 #### Data wrangling ####
-df <- df %>% as_tibble() %>% mutate(
-  ESTACION = case_when(
-    ESTACION == 'ALB' ~ 'ALBERDI',
-    ESTACION == 'Alberdi' ~ 'ALBERDI',
-    ESTACION == 'AVE' ~ 'AVELLANEDA',
-    ESTACION == 'Avellaneda' ~ 'AVELLANEDA',
-    ESTACION == 'Dellepiane Centro' ~ 'DELLEPIANE CENTRO',
-    ESTACION == 'Dellepiane Liniers' ~ 'DELLEPIANE LINIERS',
-    ESTACION == 'ILL' ~ 'ILLIA',
-    ESTACION == 'Illia' ~ 'ILLIA',
-    ESTACION == 'RET' ~ 'RETIRO',
-    ESTACION == 'Retiro' ~ 'RETIRO',
-    ESTACION == 'SAL' ~ 'SALGUERO',
-    ESTACION == 'Salguero' ~ 'SALGUERO',
-    ESTACION == 'SAR' ~ 'SARMIENTO',
-    ESTACION == 'Sarmiento' ~ 'SARMIENTO',
-    TRUE ~ as.character(ESTACION)
-  ),
-  HORA = case_when(
-    HORA == '0' ~ '00:00:00',
-    HORA == '1' ~ '01:00:00',
-    HORA == '2' ~ '02:00:00',
-    HORA == '3' ~ '03:00:00',
-    HORA == '4' ~ '04:00:00',
-    HORA == '5' ~ '05:00:00',
-    HORA == '6' ~ '06:00:00',
-    HORA == '7' ~ '07:00:00',
-    HORA == '8' ~ '08:00:00',
-    HORA == '9' ~ '09:00:00',
-    HORA == '10' ~ '10:00:00',
-    HORA == '11' ~ '11:00:00',
-    HORA == '12' ~ '12:00:00',
-    HORA == '13' ~ '13:00:00',
-    HORA == '14' ~ '14:00:00',
-    HORA == '15' ~ '15:00:00',
-    HORA == '16' ~ '16:00:00',
-    HORA == '17' ~ '17:00:00',
-    HORA == '18' ~ '18:00:00',
-    HORA == '19' ~ '19:00:00',
-    HORA == '20' ~ '20:00:00',
-    HORA == '21' ~ '21:00:00',
-    HORA == '22' ~ '22:00:00',
-    HORA == '23' ~ '23:00:00',
-    HORA == '0:00:00' ~ '00:00:00',
-    HORA == '1:00:00' ~ '01:00:00',
-    HORA == '2:00:00' ~ '02:00:00',
-    HORA == '3:00:00' ~ '03:00:00',
-    HORA == '4:00:00' ~ '04:00:00',
-    HORA == '5:00:00' ~ '05:00:00',
-    HORA == '6:00:00' ~ '06:00:00',
-    HORA == '7:00:00' ~ '07:00:00',
-    HORA == '8:00:00' ~ '08:00:00',
-    HORA == '9:00:00' ~ '09:00:00',
-    HORA == '0 days 00:00:00' ~ '00:00:00',
-    HORA == '0 days 01:00:00' ~ '01:00:00',
-    HORA == '0 days 02:00:00' ~ '02:00:00',
-    HORA == '0 days 03:00:00' ~ '03:00:00',
-    HORA == '0 days 04:00:00' ~ '04:00:00',
-    HORA == '0 days 05:00:00' ~ '05:00:00',
-    HORA == '0 days 06:00:00' ~ '06:00:00',
-    HORA == '0 days 07:00:00' ~ '07:00:00',
-    HORA == '0 days 08:00:00' ~ '08:00:00',
-    HORA == '0 days 09:00:00' ~ '09:00:00',
-    HORA == '0 days 10:00:00' ~ '10:00:00',
-    HORA == '0 days 11:00:00' ~ '11:00:00',
-    HORA == '0 days 12:00:00' ~ '12:00:00',
-    HORA == '0 days 13:00:00' ~ '13:00:00',
-    HORA == '0 days 14:00:00' ~ '14:00:00',
-    HORA == '0 days 15:00:00' ~ '15:00:00',
-    HORA == '0 days 16:00:00' ~ '16:00:00',
-    HORA == '0 days 17:00:00' ~ '17:00:00',
-    HORA == '0 days 18:00:00' ~ '18:00:00',
-    HORA == '0 days 19:00:00' ~ '19:00:00',
-    HORA == '0 days 20:00:00' ~ '20:00:00',
-    HORA == '0 days 21:00:00' ~ '21:00:00',
-    HORA == '0 days 22:00:00' ~ '22:00:00',
-    HORA == '0 days 23:00:00' ~ '23:00:00',
-    TRUE ~ as.character(HORA)
-  ),
-  HORA_FIN = case_when(
-    HORA_FIN == '0' ~ '00:00:00',
-    HORA_FIN == '1' ~ '01:00:00',
-    HORA_FIN == '2' ~ '02:00:00',
-    HORA_FIN == '3' ~ '03:00:00',
-    HORA_FIN == '4' ~ '04:00:00',
-    HORA_FIN == '5' ~ '05:00:00',
-    HORA_FIN == '6' ~ '06:00:00',
-    HORA_FIN == '7' ~ '07:00:00',
-    HORA_FIN == '8' ~ '08:00:00',
-    HORA_FIN == '9' ~ '09:00:00',
-    HORA_FIN == '10' ~ '10:00:00',
-    HORA_FIN == '11' ~ '11:00:00',
-    HORA_FIN == '12' ~ '12:00:00',
-    HORA_FIN == '13' ~ '13:00:00',
-    HORA_FIN == '14' ~ '14:00:00',
-    HORA_FIN == '15' ~ '15:00:00',
-    HORA_FIN == '16' ~ '16:00:00',
-    HORA_FIN == '17' ~ '17:00:00',
-    HORA_FIN == '18' ~ '18:00:00',
-    HORA_FIN == '19' ~ '19:00:00',
-    HORA_FIN == '20' ~ '20:00:00',
-    HORA_FIN == '21' ~ '21:00:00',
-    HORA_FIN == '22' ~ '22:00:00',
-    HORA_FIN == '23' ~ '23:00:00',
-    HORA_FIN == '0:00:00' ~ '00:00:00',
-    HORA_FIN == '1:00:00' ~ '01:00:00',
-    HORA_FIN == '2:00:00' ~ '02:00:00',
-    HORA_FIN == '3:00:00' ~ '03:00:00',
-    HORA_FIN == '4:00:00' ~ '04:00:00',
-    HORA_FIN == '5:00:00' ~ '05:00:00',
-    HORA_FIN == '6:00:00' ~ '06:00:00',
-    HORA_FIN == '7:00:00' ~ '07:00:00',
-    HORA_FIN == '8:00:00' ~ '08:00:00',
-    HORA_FIN == '9:00:00' ~ '09:00:00',
-    HORA_FIN == '0 days 00:00:00' ~ '00:00:00',
-    HORA_FIN == '0 days 01:00:00' ~ '01:00:00',
-    HORA_FIN == '0 days 02:00:00' ~ '02:00:00',
-    HORA_FIN == '0 days 03:00:00' ~ '03:00:00',
-    HORA_FIN == '0 days 04:00:00' ~ '04:00:00',
-    HORA_FIN == '0 days 05:00:00' ~ '05:00:00',
-    HORA_FIN == '0 days 06:00:00' ~ '06:00:00',
-    HORA_FIN == '0 days 07:00:00' ~ '07:00:00',
-    HORA_FIN == '0 days 08:00:00' ~ '08:00:00',
-    HORA_FIN == '0 days 09:00:00' ~ '09:00:00',
-    HORA_FIN == '0 days 10:00:00' ~ '10:00:00',
-    HORA_FIN == '0 days 11:00:00' ~ '11:00:00',
-    HORA_FIN == '0 days 12:00:00' ~ '12:00:00',
-    HORA_FIN == '0 days 13:00:00' ~ '13:00:00',
-    HORA_FIN == '0 days 14:00:00' ~ '14:00:00',
-    HORA_FIN == '0 days 15:00:00' ~ '15:00:00',
-    HORA_FIN == '0 days 16:00:00' ~ '16:00:00',
-    HORA_FIN == '0 days 17:00:00' ~ '17:00:00',
-    HORA_FIN == '0 days 18:00:00' ~ '18:00:00',
-    HORA_FIN == '0 days 19:00:00' ~ '19:00:00',
-    HORA_FIN == '0 days 20:00:00' ~ '20:00:00',
-    HORA_FIN == '0 days 21:00:00' ~ '21:00:00',
-    HORA_FIN == '0 days 22:00:00' ~ '22:00:00',
-    HORA_FIN == '0 days 23:00:00' ~ '23:00:00',
-    TRUE ~ as.character(HORA_FIN)
-  ),
-  DIA = case_when(
-    DIA == 'Lunes' ~ 'LUNES',
-    DIA == 'Martes' ~ 'MARTES',
-    DIA == 'Miercoles' ~ 'MIERCOLES',
-    DIA == 'Jueves' ~ 'JUEVES',
-    DIA == 'Viernes' ~ 'VIERNES',
-    DIA == 'Sabado' ~ 'SABADO',
-    DIA == 'Domingo' ~ 'DOMINGO',
-    TRUE ~ as.character(DIA)
-  ),
-  TIPO_VEHICULO = case_when(
-    TIPO_VEHICULO == 'Liviano' ~ 'LIVIANO',
-    TIPO_VEHICULO == 'Pesado' ~ 'PESADO',
-    TRUE ~ as.character(TIPO_VEHICULO)
-  ),
-  ESTACION = as.factor(ESTACION),
-  HORA = as.factor(HORA),
-  HORA_FIN = as.factor(HORA_FIN),
-  DIA = as.factor(DIA),
-  TIPO_VEHICULO = as.factor(TIPO_VEHICULO)
-)
-# Once the data wrangling process is completed, save the transformed dataframe 
-# into a new .csv file and use that one from now on.
-write.csv(df, file = traffic, row.names = F)
-rm(df, merged, traffic)
+df <- transform.values(df, './datasets/traffic.csv')
 
 #### Perform EDA on the traffic dataset ####
-df <- read.csv('./datasets/traffic.csv', header = T, stringsAsFactors = T)
+YoY.diff <- function(period, conditions = TRUE) {
+  #' Difference in number of cars between an year X and year X - 1.
+  #' 
+  #' @param period The list of years to be considered to compute differences.
+  #' @param conditions List of dataframe attributes and associated values to use
+  #' as filtering conditions.
+  #' @return List of differences between two consecutive years, aggregated by
+  #' hour of day.
+  #' @example 
+  #' YoY.diff(c(2008, 2009, 2010), df$attr == TRUE)
+  
+  differences <- vector('list', length(years) - 1); i <- 1
+  for(yr in years) {
+    if(yr == 2018) { break }
+    traffic.A <- df[which(df$PERIODO == yr & conditions), ]
+    traffic.B <- df[which(df$PERIODO == (yr + 1) & conditions), ]
+    traffic.A.by.hour <- traffic.A %>%
+      group_by(HORA) %>%
+      summarise(amount.of.cars = sum(CANTIDAD_PASOS))
+    traffic.B.by.hour <- traffic.B %>%
+      group_by(HORA) %>%
+      summarise(amount.of.cars = sum(CANTIDAD_PASOS))
+    differences[[i]] <- (traffic.B.by.hour$amount.of.cars - 
+                         traffic.A.by.hour$amount.of.cars)
+    i <- i + 1
+  }
+  return(differences)
+}
+
+years <- unique(df$PERIODO)
+conditions <- (df$ESTACION == 'ALBERDI' 
+               & df$FORMA_PAGO == 'INFRACCION' 
+               & df$TIPO_VEHICULO == 'PESADO')
+differences <- YoY.diff(years, conditions)
+boxplot(differences, las = 0,
+        names = c('2009-2008', '2010-2009', '2011-2010', '2012-2011',
+                  '2013-2012', '2014-2013', '2015-2014', '2016-2015',
+                  '2017-2016', '2018-2017'),
+        xlab = 'YoY difference', 
+        ylab = 'Number of cars')
