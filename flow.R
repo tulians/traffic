5f3462534773d7208dd0ad689e603123de1e9615
tulians <julianailan@outlook.com>
2018-11-24 10:05:24 -0300

    Performed differences between years, and expressed them with boxplots. First iteration on forecasting traffic 2 years ahead.

--- flow.R
+++ flow.R
@@ -4,6 +4,7 @@
 
 rm(list = ls()); gc();
 library(dplyr)
+library(forecast)
 
 setwd('~/Documents/traffic/')
 source('utils.R')
@@ -14,7 +15,14 @@ df <- standarize.criteria()
 #### Data wrangling ####
 df <- transform.values(df, './datasets/traffic.csv')
 
-#### Perform EDA on the traffic dataset ####
+#### Increase in traffic comparing consecutive years ####
+
+aggregate.by.hour_ <- function(df) {
+  return(df %>%
+           group_by(HORA) %>%
+           summarise(cars.passing.by = sum(CANTIDAD_PASOS)))
+}
+
 YoY.diff <- function(period, conditions = TRUE) {
   #' Difference in number of cars between an year X and year X - 1.
   #' 
@@ -26,28 +34,23 @@ YoY.diff <- function(period, conditions = TRUE) {
   #' @example 
   #' YoY.diff(c(2008, 2009, 2010), df$attr == TRUE)
   
-  differences <- vector('list', length(years) - 1); i <- 1
-  for(yr in years) {
-    if(yr == 2018) { break }
-    traffic.A <- df[which(df$PERIODO == yr & conditions), ]
-    traffic.B <- df[which(df$PERIODO == (yr + 1) & conditions), ]
-    traffic.A.by.hour <- traffic.A %>%
-      group_by(HORA) %>%
-      summarise(amount.of.cars = sum(CANTIDAD_PASOS))
-    traffic.B.by.hour <- traffic.B %>%
-      group_by(HORA) %>%
-      summarise(amount.of.cars = sum(CANTIDAD_PASOS))
-    differences[[i]] <- (traffic.B.by.hour$amount.of.cars - 
-                         traffic.A.by.hour$amount.of.cars)
+  differences <- vector('list', length(period) - 1); i <- 1
+  for(prd in period) {
+    if(prd == tail(years, n = 1)) { break }
+    traffic.A <- df[which(df$PERIODO == prd & conditions), ]
+    traffic.B <- df[which(df$PERIODO == (prd + 1) & conditions), ]
+    traffic.A.by.hour <- aggregate.by.hour_(traffic.A)
+    traffic.B.by.hour <- aggregate.by.hour_(traffic.B)
+    differences[[i]] <- (traffic.B.by.hour$cars.passing.by - 
+                         traffic.A.by.hour$cars.passing.by)
     i <- i + 1
   }
+  rm(traffic.A, traffic.B, prd, period, i)
   return(differences)
 }
 
 years <- unique(df$PERIODO)
-conditions <- (df$ESTACION == 'ALBERDI' 
-               & df$FORMA_PAGO == 'INFRACCION' 
-               & df$TIPO_VEHICULO == 'PESADO')
+conditions <- (df$ESTACION == 'ALBERDI')
 differences <- YoY.diff(years, conditions)
 boxplot(differences, las = 0,
         names = c('2009-2008', '2010-2009', '2011-2010', '2012-2011',
@@ -55,3 +58,23 @@ boxplot(differences, las = 0,
                   '2017-2016', '2018-2017'),
         xlab = 'YoY difference', 
         ylab = 'Number of cars')
+rm(differences, conditions, aggregate.by.hour_, YoY.diff)
+#### Can a given year traffic be predicted by the previous year traffic? ####
+t <- rbind(
+  df[which(df$PERIODO == 2016), ] %>%
+    group_by(FECHA) %>%
+    summarise(CANTIDAD_PASOS = sum(CANTIDAD_PASOS)),
+  df[which(df$PERIODO == 2017), ] %>%
+    group_by(FECHA) %>%
+    summarise(CANTIDAD_PASOS = sum(CANTIDAD_PASOS)))
+# Correct how rows are ordered by date.
+t <- t %>%
+  mutate(FECHA = as.Date(FECHA, format = '%m/%d/%Y')) %>%
+  arrange(FECHA)
+# Transform it into time-series and forecast.
+t.ts <- msts(t$CANTIDAD_PASOS, seasonal.periods = c(7, 365.25))
+fit <- stlf(t.ts)
+fc <- forecast(fit)
+plot(fc)
+t.ts.components <- decompose(t.ts)
+plot(t.ts.components)
